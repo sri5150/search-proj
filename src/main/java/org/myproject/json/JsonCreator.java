cd17b7d46aa4d638ccfbf30280f45baadd21d553
sri5150 <sri5150@yahoo.com>
2023-04-14 15:53:53 -0400

    Add files via upload

---
+++ src/main/java/org/myproject/json/JsonCreator.java
@@ -0,0 +1,4 @@
+package org.myproject.json;
+
+public class JsonCreator {
+}
---
+++ src/main/java/org/myproject/servlet/SearchServ.java
@@ -0,0 +1,36 @@
+package org.myproject.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SearchServ extends HttpServlet {
+
+    private String message;
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+
+        // Set response content type
+        response.setContentType("text/html");
+
+        String payload;
+        String query = request.getParameter("q");
+        if (query == null || query.trim().length() == 0) {
+            // create no results json
+            payload = "No results";
+        } else {
+            // return full data set
+            payload = "Full data set";
+        }
+
+        PrintWriter out = response.getWriter();
+        out.println(payload);
+    }
+
+    public void destroy() {
+        // do nothing.
+    }
+}
